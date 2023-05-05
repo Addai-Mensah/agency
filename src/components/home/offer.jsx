1bf076ff0198642e37fb23556e472ca74111a2ed
Addai-Mensah <mensahaddaai@gmail.com>
2023-05-05 12:41:22 +0100

    again

--- src/components/home/offer.jsx
+++ src/components/home/offer.jsx
@@ -3,7 +3,7 @@ import icon1 from "../../assets/icon1.png"
 import icon2 from "../../assets/icon2.png"
 import icon3 from "../../assets/icon3.png"
 import { useAnimation, motion } from "framer-motion";
-import { useInView } from "react-intersection-observer";
+
 
 
 
